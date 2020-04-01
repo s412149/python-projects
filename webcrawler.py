a42a16622dcfe507b02c05263e862d2b82d362e6
s412149 <33458587+s412149@users.noreply.github.com>
2020-04-01 17:21:32 +0200

    implemented fetching the whole document

--- webcrawler.py
+++ webcrawler.py
@@ -5,3 +5,14 @@ Created on Wed Apr  1 17:13:32 2020
 @author: agato
 """
 
+import urllib.request
+from bs4 import BeautifulSoup
+
+print('Getting staff urls...')
+
+staff_url = 'http://wa.amu.edu.pl/wa/en/staff_list'
+response = urllib.request.urlopen(staff_url)
+data = response.read()
+doc = BeautifulSoup(data, 'html.parser')
+
+print(doc)
