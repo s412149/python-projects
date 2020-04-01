3d7c5b2ddf0f85e7c292a7327f55f828a070bcce
s412149 <33458587+s412149@users.noreply.github.com>
2020-04-01 18:17:55 +0200

    Factor = wrap in main function

--- webcrawler.py
+++ webcrawler.py
@@ -9,24 +9,27 @@ import urllib.request
 from bs4 import BeautifulSoup
 import urllib.parse
 
-print('Getting staff urls...')
-
-staff_url = 'http://wa.amu.edu.pl/wa/en/staff_list'
-response = urllib.request.urlopen(staff_url)
-data = response.read()
-doc = BeautifulSoup(data, 'html.parser')
-
-staff_content = doc.find(id='tresc_wlasciwa')
-
-links = staff_content.find_all('a')
-
-urls = []
-
-for link in links:
-    if len(link.get_text()) > 1:
-        base_url = 'http://wa.amu.edu.pl'
-        url = urllib.parse.urljoin(base_url, link['href'])
-        urls.append(url)
+def main():
+    print('Getting staff urls...')
     
-print('URLs found:')
-print('\n'.join(urls))
+    staff_url = 'http://wa.amu.edu.pl/wa/en/staff_list'
+    response = urllib.request.urlopen(staff_url)
+    data = response.read()
+    doc = BeautifulSoup(data, 'html.parser')
+    
+    staff_content = doc.find(id='tresc_wlasciwa')
+    
+    links = staff_content.find_all('a')
+    
+    urls = []
+    
+    for link in links:
+        if len(link.get_text()) > 1:
+            base_url = 'http://wa.amu.edu.pl'
+            url = urllib.parse.urljoin(base_url, link['href'])
+            urls.append(url)
+        
+    print('URLs found:')
+    print('\n'.join(urls))
+    
+main()
