3c5c33fd11b2ab82bec0ce793ece7a19df70e44a
s412149 <33458587+s412149@users.noreply.github.com>
2020-04-06 12:23:13 +0200

    printing ordered lists

--- fetch_links.py
+++ fetch_links.py
@@ -5,3 +5,19 @@ Created on Sun Apr  5 23:47:27 2020
 @author: agato
 """
 
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = 'file:///C:/Users/agato/Desktop/example3.html'
+response = urllib.request.urlopen(url)
+data = response.read()
+doc = BeautifulSoup(data, 'html.parser')
+
+li = doc.select('ol > li')
+i = 1
+for link in li:
+    print(str(i) +'. ' + link.get_text())
+    i += 1
+
+
+
