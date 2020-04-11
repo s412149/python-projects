83f5f6c2f438951f2484fd86e45ca5780a27e578
s412149 <33458587+s412149@users.noreply.github.com>
2020-04-11 14:29:26 +0200

    implemented piratetrans

--- translate.py
+++ translate.py
@@ -5,11 +5,20 @@ Created on Sat Apr 11 14:10:17 2020
 @author: agato
 """
 from googletrans import Translator
+import requests
 
-def main():
+def googletrans():
     translator = Translator()
     result = translator.translate('Jak się masz?',dest='es')
     print(result.text)
     
     
-main()
+def piratetrans(text):
+    url = 'https://api.funtranslations.com/translate/pirate.json'
+    data = {'text': text}
+    
+    response = requests.post(url, data=data)
+    print(response.text)
+    
+piratetrans('Hello, sir')
+    
