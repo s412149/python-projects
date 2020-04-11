0c17fabe310dde1e8a60de6dcac7fadde5650da9
s412149 <33458587+s412149@users.noreply.github.com>
2020-04-11 14:17:49 +0200

    created function main

--- translate.py
+++ translate.py
@@ -4,4 +4,12 @@ Created on Sat Apr 11 14:10:17 2020
 
 @author: agato
 """
+from googletrans import Translator
 
+def main():
+    translator = Translator()
+    result = translator.translate('Jak się masz?',dest='es')
+    print(result.text)
+    
+    
+main()
